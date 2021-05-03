a1fb604bc398324ca20f89bab972ab981336d8b7
Primanda Sayarizki <primanda.sayarizki8@gmail.com>
2021-05-03 15:50:06 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/com/d3if2070/galerihewan/model/Hewan.kt
+++ app/src/main/java/com/d3if2070/galerihewan/model/Hewan.kt
@@ -1,8 +1,10 @@
 package com.d3if2070.galerihewan.model
 
+import com.d3if2070.galerihewan.R
+
+
 data class Hewan(
         val nama: String,
         val namaLatin: String,
-        val jenis: String,
-        val imageResId: Int
+        val imageResId: Int = R.drawable.angsa
 )
--- app/src/main/java/com/d3if2070/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/com/d3if2070/galerihewan/network/HewanApiService.kt
@@ -1,19 +1,26 @@
 package com.d3if2070.galerihewan.network
 
+import com.d3if2070.galerihewan.model.Hewan
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://dif.indraazimi.com/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface HewanApiService {
     @GET("listhewan.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {
